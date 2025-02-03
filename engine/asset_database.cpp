e91825a3093aae30bd570210f8f8704d225a1162
Washu Hakubi <ryoohki@gmail.com>
2025-02-03 15:04:24 -0800

    Minor cleanup

--- engine/asset_database.cpp
+++ engine/asset_database.cpp
@@ -4,9 +4,6 @@
  *  Distributed under the Boost Software License, Version 1.0. (See accompanying
  *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
-
-// #include <folly/coro/Task.h>
-
 #include "engine/asset_database.h"
 #include "engine/i_component_parser.h"
 #include "engine/i_file_provider.h"
